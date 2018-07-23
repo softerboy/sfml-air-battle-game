aad0229fc07b346579337d13a41334b41c371f1d
Umarov Faxriddin <f.umarov@outlook.com>
2018-07-23 14:11:02 +0500

    Moving a circle (player)

--- src/game.cpp
+++ src/game.cpp
@@ -9,6 +9,8 @@ Game::Game() :
     mPlayer.setRadius(40.f);
     mPlayer.setPosition(100.f, 100.f);
     mPlayer.setFillColor(sf::Color::Cyan);
+
+    mIsMovingUp = mIsMovingDown = mIsMovingLeft = mIsMovingRight = false;
 }
 
 void Game::run()
@@ -26,14 +28,36 @@ void Game::processEvents()
     sf::Event event;
     while(mWindow.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed)
+        switch (event.type)
+        {
+        case sf::Event::KeyPressed:
+            handlePlayerInput(event.key.code, true);
+            break;
+
+        case sf::Event::KeyReleased:
+            handlePlayerInput(event.key.code, false);
+            break;
+
+        case sf::Event::Closed:
             mWindow.close();
+            break;
+        }
     }
 }
 
 void Game::update()
 {
+    sf::Vector2f movement(0.f, 0.f);
+    if (mIsMovingUp)
+        movement.y -= 1.0f;
+    if (mIsMovingDown)
+        movement.y += 1.0f;
+    if (mIsMovingLeft)
+        movement.x -= 1.0f;
+    if (mIsMovingRight)
+        movement.x += 1.0f;
 
+    mPlayer.move(movement);
 }
 
 void Game::render()
@@ -41,6 +65,18 @@ void Game::render()
     mWindow.clear();
     mWindow.draw(mPlayer);
     mWindow.display();
+}
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    if (key == sf::Keyboard::W)
+        mIsMovingUp = isPressed;
+    else if (key == sf::Keyboard::S)
+        mIsMovingDown = isPressed;
+    else if (key == sf::Keyboard::A)
+        mIsMovingLeft = isPressed;
+    else if (key == sf::Keyboard::D)
+        mIsMovingRight = isPressed;
 }
 
 
--- src/game.h
+++ src/game.h
@@ -13,10 +13,17 @@ private:
     void processEvents();
     void update();
     void render();
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 
 private:
     sf::RenderWindow mWindow;
     sf::CircleShape mPlayer;
+
+private:
+    bool mIsMovingUp;
+    bool mIsMovingDown;
+    bool mIsMovingLeft;
+    bool mIsMovingRight;
 };
 
 #endif // GAME_H
